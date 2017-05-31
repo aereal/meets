afd4e478c0cf258963ead61714dcf828cc4c6535
aereal <aereal@aereal.org>
2017-05-31 11:48:03 +0900

    More typings

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@
 import Person from '../models/Person';
 
 let meetingId = 0;
-export const addMeeting = () => {
+export const addMeeting = (): AddMeetingAction => {
   debugger;
   return ({
     type: 'ADD_MEETING',
@@ -11,11 +11,23 @@ export const addMeeting = () => {
     members: [],
   });
 };
+export type AddMeetingAction = {
+  type: 'ADD_MEETING',
+  id: number,
+  members: Person[],
+};
 
-export const addMember = (meetingId: number, member: Person) => {
+export const addMember = (meetingId: number, member: Person): AddMemberAction => {
   return ({
     type: 'ADD_MEMBER',
     id: meetingId,
     member: member,
   });
 };
+export type AddMemberAction = {
+  type: 'ADD_MEMBER',
+  id: number,
+  member: Person,
+};
+
+export type Action = AddMeetingAction | AddMemberAction;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,16 @@
 
 import { combineReducers } from 'redux';
 
+import Person from '../models/Person';
+import type { Action, AddMemberAction } from '../actions';
+
+type State = {
+  id: number,
+  members: Person[],
+}[];
+
 const App = combineReducers({
-  meetings(state = [], action) {
+  meetings(state: State = [], action: Action) {
     switch (action.type) {
       case 'ADD_MEETING':
         return [
@@ -14,11 +22,12 @@ const App = combineReducers({
           }
         ];
       case 'ADD_MEMBER':
+        const addMemberAction = (action: AddMemberAction);
         return state.map(meeting => {
-          return (meeting.id === action.id) ?
+          return (meeting.id === addMemberAction.id) ?
             {
               id: meeting.id,
-              members: meeting.members.concat([action.member]),
+              members: meeting.members.concat([addMemberAction.member]),
             } :
             meeting;
         });
