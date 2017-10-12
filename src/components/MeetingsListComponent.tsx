e9b7348c5a873ecacae2f8467981c325a26233dc
aereal <aereal@aereal.org>
2017-10-12 16:47:31 +0900

    as PureComponent

--- src/components/MeetingsListComponent.tsx
+++ src/components/MeetingsListComponent.tsx
@@ -13,36 +13,39 @@ interface MeetingsListProps extends CallbackProps {
   meetings: Meeting[];
 }
 
-const renderPlaceholder = () => {
-  return (
-    <div className="card-panel">
-      <p>
-        右下の
-        {" "}
-        <button className="btn-floating btn-large red waves-effect waves-light">
-          <i className="material-icons">add</i>
-        </button>
-        {" "}
-        を押してMTGを追加します。
-      </p>
-      <p>
-        <button className="btn-floating green">
-          <i className="material-icons">reorder</i>
-        </button>
-        {" "}
-        を押すと参加する人が多い順にMTGを並び替えます。<br/>
-        場所をまたいでChromeboxを使う必要があるとより上に現れます。
-      </p>
-    </div>
-  );
-};
-
-const MeetingsListComponent = ({ meetings = [], onAddMember }: MeetingsListProps) => {
-  return meetings.length === 0 ?
-    renderPlaceholder() :
-    (
-      <div>{meetings.map(m => <MeetingComponent key={m.id} meetingId={m.id} members={m.members} onClick={onAddMember} />)}</div>
+class MeetingsListComponent extends React.PureComponent<MeetingsListProps, {}> {
+  renderPlaceholder() {
+    return (
+      <div className="card-panel">
+        <p>
+          右下の
+          {" "}
+          <button className="btn-floating btn-large red waves-effect waves-light">
+            <i className="material-icons">add</i>
+          </button>
+          {" "}
+          を押してMTGを追加します。
+        </p>
+        <p>
+          <button className="btn-floating green">
+            <i className="material-icons">reorder</i>
+          </button>
+          {" "}
+          を押すと参加する人が多い順にMTGを並び替えます。<br/>
+          場所をまたいでChromeboxを使う必要があるとより上に現れます。
+        </p>
+      </div>
     );
-};
+  };
+
+  render() {
+    const { meetings, onAddMember } = this.props;
+    return meetings.length === 0 ?
+      this.renderPlaceholder() :
+      (
+        <div>{meetings.map(m => <MeetingComponent key={m.id} meetingId={m.id} members={m.members} onClick={onAddMember} />)}</div>
+      );
+  }
+}
 
 export default MeetingsListComponent;
